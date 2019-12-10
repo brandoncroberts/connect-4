be122696fffd0e6832e490ed86247305f5b82ccc
brandon roberts <88.brandoncr@gmail.com>
2019-12-10 11:28:02 +0100

    Add Board component to app

---
+++ src/components/board/board.component.jsx
@@ -0,0 +1,153 @@
+import React from "react";
+import { BoardContainer } from "./board.styles";
+
+const Board = () => {
+  return (
+    <BoardContainer class="board">
+      <div class="column">
+        <div class="slots row0">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row1">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row2">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row3">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row4">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row5">
+          <div class="hole"></div>
+        </div>
+      </div>
+
+      <div class="column">
+        <div class="slots row0">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row1">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row2">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row3">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row4">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row5">
+          <div class="hole"></div>
+        </div>
+      </div>
+
+      <div class="column">
+        <div class="slots row0">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row1">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row2">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row3">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row4">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row5">
+          <div class="hole"></div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="slots row0">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row1">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row2">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row3">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row4">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row5">
+          <div class="hole"></div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="slots row0">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row1">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row2">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row3">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row4">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row5">
+          <div class="hole"></div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="slots row0">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row1">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row2">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row3">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row4">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row5">
+          <div class="hole"></div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="slots row0">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row1">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row2">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row3">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row4">
+          <div class="hole"></div>
+        </div>
+        <div class="slots row5">
+          <div class="hole"></div>
+        </div>
+      </div>
+    </BoardContainer>
+  );
+};
+
+export default Board;
---
+++ src/components/board/board.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const BoardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 635px;
+  width: 740px;
+  background: blue;
+`;
