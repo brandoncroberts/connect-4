30a1d0ba646726ad623e5907077bfe196bededb0
brandon roberts <8brandon@protonmail.com>
2019-12-18 05:45:24 -0800

    Remove custom button

--- src/components/custom-button/customButton.component.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Button } from "./customButton.styles";
-
-const CustomButton = ({ title }) => {
-  return <Button>{title}</Button>;
-};
-
-export default CustomButton;
